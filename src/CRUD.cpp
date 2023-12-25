9a1e1cb48ff77d56787746fc00ebfc5692ef5e29
Luieitalian <enderbud125@hotmail.com>
2023-12-25 18:32:37 +0300

    finito

--- src/CRUD.cpp
+++ src/CRUD.cpp
@@ -1,5 +1,6 @@
 #include "C:/Program Files/PostgreSQL/16/include/libpq-fe.h"
 #include <iostream>
+#include <string>
 
 void print_table(const std::string &table) {
   for (size_t i = 0; i < table.size(); i++) {
@@ -38,4 +39,46 @@ std::string get_table(std::string table_name, PGconn *conn) {
   }
 
   return str;
+}
+
+void insert_row(std::string row_data, PGconn *conn, std::string table_name,
+                std::string table_columns) {
+  std::string query = "insert into ";
+  query.append(table_name);
+  query.append(" (");
+  query.append(table_columns);
+  query.append(") ");
+  query.append("values (");
+  query.append(row_data);
+  query.append(");");
+
+  PGresult *res = PQexec(conn, query.c_str());
+  std::cout << PQerrorMessage(conn);
+}
+
+void delete_row(PGconn *conn, std::string table_name, std::string condition) {
+  std::string query = "delete from ";
+  query.append(table_name);
+  query.append(" where ");
+  query.append(condition);
+  query.append(";");
+
+  PGresult *res = PQexec(conn, query.c_str());
+  std::cout << PQerrorMessage(conn);
+}
+
+void update_row(PGconn *conn, std::string table_name, std::string column,std::string condition, std::string data){
+  std::string query = "update ";
+  query.append(table_name);
+  query.append(" set ");
+  query.append(column);
+  query.append(" = ");
+  query.append(data);
+  query.append(" where ");
+  query.append(condition);
+  query.append(";");
+
+
+  PGresult *res = PQexec(conn, query.c_str());
+  std::cout << PQerrorMessage(conn);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <locale.h>
 #include <ostream>
+#include <process.h>
 #include <string>
 
 // Online_Learning_Platform
@@ -12,7 +13,7 @@ int main() {
 
   const char *conn_info =
       "host=localhost user=postgres password=1234 port=5432 "
-      "dbname=test connect_timeout=10";
+      "dbname=Online_Learning_Platform connect_timeout=10";
 
   PGconn *conn = PQconnectdb(conn_info);
 
@@ -22,11 +23,66 @@ int main() {
   }
   std::cout << "CONNECTION OK!\n";
 
-  std::string employees_table = get_table("\"public\".\"categories\"", conn);
-  print_table(employees_table);
-  // const char *query = "select * from \"public\".\"employees\";";
-  // PGresult *res = PQexec(conn, query);
-  // std::cout << PQgetvalue(res, 0, 2) << std::endl;
-
+  char secenek;
+  while (true) {
+    std::cout << "1. insert_row\n";
+    std::cout << "2. print_table\n";
+    std::cout << "3. update_row\n";
+    std::cout << "4. delete_row\n";
+    std::cout << "5. exit\n";
+    std::cin >> secenek;
+    std::string table_name;
+    std::string row_data;
+    std::string table_columns;
+    std::string column;
+    std::string condition;
+    std::string data;
+    switch (secenek) {
+    case '1':
+      std::cout << "\ntable_name : ";
+      std::cin >> table_name;
+      std::cout << "\ntable_columns :";
+      std::cin >> table_columns;
+      std::cout << "row_data : ";
+      std::cin >> row_data;
+      insert_row(row_data, conn, table_name, table_columns);
+      break;
+    case '2':
+      std::cout << "\ntable_name : ";
+      std::cin >> table_name;
+      print_table(get_table(table_name, conn));
+      break;
+    case '3':
+      std::cin.ignore();
+      std::cout << "\ntable_name : ";
+      std::getline(std::cin, table_name);
+      std::cout << "\ncolumn : ";
+      std::getline(std::cin, column);
+      std::cout << "\ncondition : ";
+      std::getline(std::cin, condition);
+      std::cout << "\ndata : ";
+      std::getline(std::cin, data);
+      update_row(conn, table_name, column, condition, data);
+      break;
+    case '4':
+      std::cin.ignore();
+      std::cout << "\ntable_name : ";
+      std::getline(std::cin, table_name);
+      std::cout << "\ncondition : ";
+      std::getline(std::cin, condition);
+      delete_row(conn, table_name, condition);
+      break;
+    case '5':
+      exit(1);
+      break;
+    }
+  }
+  // insert_row("default,1,'Bugün kurs yayınlanmayacaktır!','2023-12-25'", conn,
+  // "\"public\".\"announcement\"",
+  // "announcement_id,instructor_id,message,publish_date"); std::string table =
+  // get_table("rating", conn); print_table(table); delete_row(conn,
+  // "announcement", "announcement_id=2 and instructor_id=1"); update_row(conn,
+  // "announcement", "message", "announcement_id=11", "'Yarın C++ kursu
+  // geliyor!'");
   return 0;
 }
